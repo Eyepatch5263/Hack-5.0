fefc7bc90e6d52bca7639a873e7de98bb52f0539
Eyepatch5263 <pratyushpragyey@gmail.com>
2025-02-19 00:35:18 +0530

    completed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -47,6 +47,8 @@ const Footer = () => {
                         <ul className="space-y-3">
                             <li><a className="text-gray-400 cursor-pointer text-lg hover:text-red-400 transition-colors">Getting Started</a></li>
                             <li><a href="https://discord.com/invite/GUSdHK8x" className="text-gray-400 text-lg hover:text-red-400 transition-colors">Discord</a></li>
+                            <li><a href="/coc" className="text-gray-400 text-lg hover:text-red-400 transition-colors">Code of Conduct</a></li>
+                            <li><a className="text-gray-400 text-lg hover:text-red-400 transition-colors">Hacker's Guide</a></li>
                         </ul>
                     </div>
 
